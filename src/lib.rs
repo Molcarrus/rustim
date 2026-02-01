13268d3cde7aa4e4f7878e4e74357533aa5ada09
Molcarrus <lenovolaptop.1202023@gmail.com>
2026-02-02 01:28:36 +0530

    mobject

---
+++ src/lib.rs
@@ -0,0 +1,5 @@
+pub mod objects;
+
+pub type Point = nalgebra::Point2<f64>;
+pub type Vector = nalgebra::Vector2<f64>;
+pub type Color = palette::Srgba;
---
+++ src/objects/mobject.rs
@@ -0,0 +1,27 @@
+use crate::{Color, Point};
+
+pub trait Mobject: Clone + Send + Sync {
+    fn center(&self) -> Point;
+    
+    fn shift(&mut self, delta: (f64, f64));
+    
+    fn scale(&mut self, factor: f64);
+    
+    fn rotaate(&mut self, angle: f64);
+    
+    fn set_fill(&mut self, color: Color);
+    
+    fn set_stroke(&mut self, color: Color, width: f64);
+    
+    fn to_path(&self) -> Vec<PathCommand>;
+    
+    fn interpolate(&self, other: &Self, t: f64) -> Self;
+}
+
+#[derive(Clone, Debug)]
+pub enum PathCommand {
+    MoveTo(Point),
+    LineTo(Point),
+    CurveTo(Point, Point, Point),
+    Close,
+}
